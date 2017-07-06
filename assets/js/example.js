26af713254762727bfbc96d9bd4c8389d69b894e
TC McCarthy <tc.mccarthy1@gmail.com>
2017-07-06 12:42:27 -0400

    Updates example

--- assets/js/example.js
+++ assets/js/example.js
@@ -2,14 +2,30 @@ $(function () {
 	$("video").smartVideo({
 		autoPlay: true,
 		chapters: [{
-			seconds: 5,
-			title: "Chapter 1",
-			description: "Look at the pretty colors!"
-		}, {
-			seconds: 27,
-			title: "Title Screen",
-			description: "This is where the title finally shows up."
-		}],
+				seconds: 1,
+				title: "Chapter 1",
+				description: "Look at the pretty colors!"
+			},
+			{
+				seconds: 16,
+				title: "Chapter 2",
+				description: "Humor: Bird gets knocked out"
+			},
+			{
+				seconds: 27,
+				title: "Chapter 3: Title Screen",
+				description: "This is where the title finally shows up."
+			}, {
+				seconds: 41,
+				title: "Chapter 4: The awakening",
+				description: "Our hero awakens from his slumber"
+			},
+			{
+				seconds: 56,
+				title: "Chapter 5: Starting the day",
+				description: "After a nice neck crack, Buck sets out"
+			}
+		],
 		mode: "fullscreen"
 	});
 });
